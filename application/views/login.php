dd6280e93ffb7b6a4b3336756a9622abab2e7eb4
clincepro@gmail.com <clincepro@gmail.com>
2018-09-11 18:33:09 +0530

    alt added

--- application/views/login.php
+++ application/views/login.php
@@ -14,7 +14,7 @@
         <div class="website-logo">
             <a href="index.html">
                 <div class="logo">
-                    <img class="logo-size" src="<?php echo base_url(); ?>images/logo-light.svg">
+                    <img class="logo-size" src="<?php echo base_url(); ?>images/logo-light.svg" alt="">
                 </div>
             </a>
         </div>
